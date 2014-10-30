dd29f90d62c604e6f9111a0d95e014322ab782d5
FireSpace <FireSpace66@gmail.com>
2014-10-30 04:51:13 +0300

    boolean printing fix

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <string>
+
 #include "Operations.hpp"
+
+std::string to_string(bool b)
+{
+	return b ? "True" : "False";
+}
 
 int main()
 {
-	Pointer<Boolean> p = Eq(Add((Inc(Zero)), Inc(Inc(Zero))), Mul(Inc(Inc(Zero)), Inc(Zero)));
+	Pointer<Boolean> p = Eq(Add((Inc(Zero)), Inc(Inc(Zero))), Mul(Inc(Inc(Inc(Zero))), Inc(Zero)));
 	std::cout << p->toString() << std::endl;
-	std::cout << p->calc() << std::endl;
+	std::cout << to_string(p->calc()) << std::endl;
 }
