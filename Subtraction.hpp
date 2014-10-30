9e982eeed1e41321d2490968ec78f66617003ce0
FireSpace <FireSpace66@gmail.com>
2014-10-30 04:22:37 +0300

    add subtraction (?)

---
+++ Subtraction.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Expressions.hpp"
+
+#include <memory>
+
+// Subtraction :: Object -> Object -> Object
+class Subtraction : public Binary<Object, Object, Object>
+{
+public:
+	Subtraction(Pointer<Object> arg1, Pointer<Object> arg2)
+		: Binary<Object, Object, Object>(arg1, arg2) {}
+
+
+	virtual Object calc() const override;
+	virtual std::string toString() const override;
+};
+
+Object Subtraction::calc() const
+{
+	return (std::get<0>(args)->calc() - std::get<1>(args)->calc());
+}
+
+std::string Subtraction::toString() const
+{
+	return "(" + std::get<0>(args)->toString() + "-" + std::get<1>(args)->toString() + ")";
+}
+
+Pointer<Object> Subtract(Pointer<Object> arg1, Pointer<Object> arg2)
+{
+	return std::make_shared<Subtraction>(arg1, arg2);
+}
