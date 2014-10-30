ef5ef3112dc4d33aeb82205f7cdda31955ee7469
FireSpace <FireSpace66@gmail.com>
2014-10-30 04:25:13 +0300

    add all operations include to header Operations.hpp

---
+++ Operations.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Addition.hpp"
+#include "Conjunction.hpp"
+#include "Disjunction.hpp"
+#include "Equal.hpp"
+#include "Implication.hpp"
+#include "Increment.hpp"
+#include "Multiplication.hpp"
+#include "Negation.hpp"
+#include "Subtraction.hpp"
+#include "Variable.hpp"
