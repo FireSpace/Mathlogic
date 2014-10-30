25144faaa84788c2c19bf1419afd13d9b0924484
FireSpace <FireSpace66@gmail.com>
2014-10-30 21:08:48 +0300

    right way to printing booleans

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-#include <string>
-
 #include "Operations.hpp"
-
-std::string to_string(bool b)
-{
-	return b ? "True" : "False";
-}
 
 int main()
 {
 	Pointer<Boolean> p = Eq(Add((Inc(Zero)), Inc(Inc(Zero))), Mul(Inc(Inc(Inc(Zero))), Inc(Zero)));
 	std::cout << p->toString() << std::endl;
-	std::cout << to_string(p->calc()) << std::endl;
+	std::cout << std::boolalpha << p->calc() << std::endl;
 }
