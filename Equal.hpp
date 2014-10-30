371adcca37a1efa0e91f6b6cea6ba2d84f4e546a
FireSpace <FireSpace66@gmail.com>
2014-10-30 04:20:09 +0300

    add equal

---
+++ Equal.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Expressions.hpp"
+
+#include <memory>
+
+// Equal :: Object -> Object -> Boolean
+class Equal : public Binary<Boolean, Object, Object>
+{
+public:
+	Equal(Pointer<Object> arg1, Pointer<Object> arg2)
+		: Binary<Boolean, Object, Object>(arg1, arg2) {}
+
+
+	virtual Boolean calc() const override;
+	virtual std::string toString() const override;
+};
+
+Boolean Equal::calc() const
+{
+	return std::get<0>(args)->calc() == std::get<1>(args)->calc();
+}
+
+std::string Equal::toString() const
+{
+	return "(" + std::get<0>(args)->toString() + "=" + std::get<1>(args)->toString() + ")";
+}
+
+Pointer<Boolean> Eq(Pointer<Object> arg1, Pointer<Object> arg2)
+{
+	return std::make_shared<Equal>(arg1, arg2);
+}
