f6119991077522b6a733870a3608fe06bc8ded5e
FireSpace <FireSpace666@gmail.com>
2014-11-01 17:20:08 +0300

    fix naming

--- Quantifier.hpp
+++ Quantifier.hpp
@@ -3,6 +3,7 @@
 #include "Expressions.hpp"
 #include "Function.hpp"
 #include "Variable.hpp"
+#include "Name.hpp"
 
 #include <functional>
 #include <memory>
@@ -13,20 +14,20 @@ template <bool type, typename ... ArgsT>
 class Quantifier : public Function<ArgsT...>
 {
 private:
-    ObjectVar var;
+    Name var;
     const std::function<Boolean(ArgsT...)> defaultFunc = [](ArgsT...){ return true; };
 public:
-    Quantifier(ObjectVar var, Pointer<ArgsT>... args)
-        : Function<ArgsT...>("", defaultFunc, args...)
+    Quantifier(Name var, Pointer<ArgsT>... args)
+        : Function<ArgsT...>(Name(""), defaultFunc, args...)
         , var(var) {}
 
-    Quantifier(ObjectVar var, std::function<Boolean(ArgsT...)> func, Pointer<ArgsT>... args)
-        : Function<ArgsT...>("", func, args...)
+    Quantifier(Name var, std::function<Boolean(ArgsT...)> func, Pointer<ArgsT>... args)
+        : Function<ArgsT...>(Name(""), func, args...)
         , var(var) {}
 
     virtual std::string toString() const override
     {
-        return (type ? "@" : "?") + var.toString() + "(" + this->caller(typename gens<sizeof...(ArgsT)>::type()) + ")";
+        return (type ? "(@" : "(?") + var.getName() + "(" + this->caller(typename gens<sizeof...(ArgsT)>::type()) + "))";
     }
 
     virtual Boolean calc() const override
@@ -45,23 +46,23 @@ using Exist = Quantifier<false, ArgsT...>;
 template <typename ... ArgsT>
 Pointer<Boolean> QuantForall(std::string name, Pointer<ArgsT>... args)
 {
-    return std::make_shared<Quantifier<true, ArgsT...>>(ObjectVar(Name(name)), args...);
+    return std::make_shared<Quantifier<true, ArgsT...>>((Name(name)), args...);
 }
 
 template <typename ... ArgsT>
 Pointer<Boolean> QuantExist(std::string name, std::function<Boolean(ArgsT...)> func, Pointer<ArgsT>... args)
 {
-    return std::make_shared<Quantifier<false, ArgsT...>>(ObjectVar(Name(name)), func, args...);
+    return std::make_shared<Quantifier<false, ArgsT...>>((Name(name)), func, args...);
 }
 
 template <typename ... ArgsT>
 Pointer<Boolean> QuantExist(std::string name, Pointer<ArgsT>... args)
 {
-    return std::make_shared<Quantifier<false, ArgsT...>>(ObjectVar(Name(name)), args...);
+    return std::make_shared<Quantifier<false, ArgsT...>>((Name(name)), args...);
 }
 
 template <typename ... ArgsT>
 Pointer<Boolean> QuantForall(std::string name, std::function<Boolean(ArgsT...)> func, Pointer<ArgsT>... args)
 {
-    return std::make_shared<Quantifier<true, ArgsT...>>(ObjectVar(Name(name)), func, args...);
+    return std::make_shared<Quantifier<true, ArgsT...>>((Name(name)), func, args...);
 }
