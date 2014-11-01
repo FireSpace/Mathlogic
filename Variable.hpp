241c8ab9be13c1b02f87896335c4c297daad3fa2
FireSpace <FireSpace666@gmail.com>
2014-11-01 03:59:54 +0300

    delete class variable. add ProposVar and ObjectVar as 0-arn func or term

--- Variable.hpp
+++ Variable.hpp
@@ -1,23 +1,67 @@
 #pragma once
 
 #include "Expressions.hpp"
+#include "Name.hpp"
+#include "Function.hpp"
+#include "Term.hpp"
+
+class ProposVar : Function<>
+{
+public:
+	ProposVar(Name name, Boolean var)
+			: name(name)
+			, func([var](){return var;}) {}
+
+	ProposVar(std::string name, Boolean var)
+			: name(name)
+			, func([var](){return var;}) {}
+
+	ProposVar(Boolean var)
+			: func([var](){return var;}) {}
+
+	virtual std::string toString() const override
+	{
+		return "(" + name.getName().empty() ? std::to_string(func()) : name.getName() + ")";
+	}
+};
+
+class ObjectVar : Term<>
+{
+public:
+	ObjectVar(Name name, Object var)
+			: name(name)
+			, func([var](){return var;}) {}
+
+	ObjectVar(std::string name, Object var)
+			: name(name)
+			, func([var](){return var;}) {}
+
+	ObjectVar(Object var)
+			: func([var](){return var;}) {}
+
+	virtual std::string toString() const override
+	{
+		return "(" + name.getName().empty() ? std::to_string(func()) : name.getName() + ")";
+	}
+};
 
 //Variable (maybe constant)
 //Type — variable type
 //True, False and Zero constant (nullary function)
-template <typename Type>
+/*template <typename Type>
 class Variable : public Expression<Type>
 {
 protected:
 	Type var;
-	std::string name;
+	Name name;
 
 	bool init;
 
 public:
 	Variable(Type var)
 			: var(var)
-			  , init(true) {}
+			, name("")
+			, init(true) {}
 
 	Variable(std::string name)
 			: name(name) {}
@@ -37,10 +81,11 @@ public:
 	virtual std::string toString() const override
 	{
 		if (name.empty()) return "(" + std::to_string(var) + ")";
-		else return "(" + name + ")";
+		else return "(" + name.getName() + ")";
 	}
 };
+*/
 
-const Pointer<Boolean> True = std::make_shared<Variable<Boolean>>(true, "True");
-const Pointer<Boolean> False = std::make_shared<Variable<Boolean>>(false, "False");
-const Pointer<Object> Zero = std::make_shared<Variable<Object>>(0);
+const Pointer<Boolean> True = std::make_shared<ProposVar>("True", true);
+const Pointer<Boolean> False = std::make_shared<ProposVar>("False", false);
+const Pointer<Object> Zero = std::make_shared<ObjectVar>(0);
